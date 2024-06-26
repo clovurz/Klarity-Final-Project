97bb80aa85a025b362a9c26702a70f2c9e188c9b
Chloe Wu <chloewu0603@gmail.com>
2024-06-26 08:37:51 -0700

    Initial Commit

---
+++ Klarity Final Project/Klarity_Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Klarity_Final_ProjectApp.swift
+//  Klarity Final Project
+//
+//  Created by chloe wu on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Klarity_Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
